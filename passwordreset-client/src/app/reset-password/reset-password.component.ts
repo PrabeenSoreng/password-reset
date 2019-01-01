0e62dbb19e689786173c83edb7582855c6211380
Prabeen <prabeensoreng2119@gmail.com>
2019-01-01 10:41:29 +0530

    adding validation to reset password form

--- passwordreset-client/src/app/reset-password/reset-password.component.ts
+++ passwordreset-client/src/app/reset-password/reset-password.component.ts
@@ -15,11 +15,31 @@ export class ResetPasswordComponent implements OnInit {
     this.resetPwdForm = this.fb.group({
       password: ['', Validators.required],
       cpassword: ['', Validators.required]
-    });
+    }, {validator: this.validatePassword.bind(this)});
+  }
+
+  get password() {
+    return this.resetPwdForm.get('password');
+  }
+
+  get cpassword() {
+    return this.resetPwdForm.get('cpassword');
   }
 
   onSubmit() {
-    
+    console.log(this.resetPwdForm);
   }
 
+  validatePassword(pwdFormGroup: FormGroup) {
+    const password = pwdFormGroup.get('password').value;
+    const cpassword = pwdFormGroup.get('cpassword').value;
+
+    if(cpassword.length === 0) return null;
+    if(password !== cpassword) {
+      return {
+        doesNotMatch: true
+      };
+    }
+    return null;
+  }
 }
